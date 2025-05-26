f103800311830f63eaae28e7f1c9e53278af3665
swaghero1 <irisjuhraaa@gmail.com>
2025-05-26 20:35:22 +0800

    Adding hire button

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php 
+
+$fullName = $_POST['fullName']; 
+$email = $_POST['email'];  
+$phoneNumber = $_POST['phoneNumber']; 
+
+$facebook = $_POST['facebook']; 
+$message = $_POST['message']; 
+
+?>
+
